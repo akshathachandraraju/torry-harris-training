0e67a219bc39265cd963cec590957ac2fd0b8126
akshathachandraraju <89825934+akshathachandraraju@users.noreply.github.com>
2021-09-20 17:50:04 +0530

    Add files via upload

---
+++ THDemo/src/ArrayTask.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ArrayTask {
+    public static void main(String[] args) {
+        int i;
+        float mid = 0.0f;
+        int[] ar = new int[]{1,2,3,4,5};
+            mid = ar.length/ 2;
+            System.out.println("mid :"+mid);
+        }
+    }
+
+
+        /* pattern
+        int n, x = 1;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(x + "\t");
+                x++;
+            }
+            System.out.println();
+        }
+    }
+}*/
